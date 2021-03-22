080727dd7f1894e3d8486e2a02b7ef45f8a81f1b
aanac <aanacif@gmail.com>
2021-03-22 09:37:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Chuck Norris Random Jokes Generator
+
+Hey there, testing the use of API's with a random joke generator.
+
+The database used on this project can be found at [ICNDB](http://www.icndb.com/)
+
+_Note: Used Sass stylesheets boilerplate mostly for the purpose of testing, it's great. You can find the link below._
+
+## Sass 7-1 architecture pattern
+
+https://github.com/KittyGiraudel/sass-boilerplate
+
+Also used Sass-mq for all the media queries [Sass-MQ](https://github.com/sass-mq/sass-mq)
---
+++ dist/js/app.js
@@ -0,0 +1,47 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+  // get user input
+  const number = document.getElementById('number').value;
+  const firstName = document.getElementById('firstName').value;
+  const lastName = document.getElementById('lastName').value;
+  // create new request object
+  const api = new XMLHttpRequest();
+
+  // send request  
+  if (number > 0) {
+    if (firstName && lastName) {
+      api.open('GET', `http://api.icndb.com/jokes/random/${number}?firstName=${firstName}&lastName=${lastName}`, true);
+    } else {
+      api.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+    }
+  } else {
+    alert('Number must be greather than 0');
+  }
+
+  // handle request  
+  api.onload = function () {
+    // checking if response is successful (200)
+    if (api.status === 200) {
+      // turn raw data into object
+      const result = JSON.parse(this.responseText);
+      let jokeList = '';
+      if (result.type == 'success') {
+        result.value.forEach(function (joke) {
+          jokeList += `<li>${joke.joke}</li>`;
+        });
+      } else {
+        jokeList += '<li>Error</li>';
+      }
+      document.querySelector('.jokeList').innerHTML = jokeList;
+    }
+  }
+  api.send();
+  e.preventDefault();
+}
+
+// Informational responses (100–199)
+// Successful responses (200–299)
+// Redirects (300–399)
+// Client errors (400–499)
+// Server errors (500–599)
